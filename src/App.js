7430090e07ae164e4ffb5350e23ea3c14ed053db
Serey <zin4ik86@gmail.com>
2019-09-16 01:32:39 +0300

    ctuctur content & Dialogs page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Header from './components/Header/Header';
 import SiteBar from './components/SiteBar/SiteBar';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import Dialogs from './components/Dialogs/Dialogs';
 
 
 
 function App() {
   return (
-   <div className="wrapp">
-    <Header/>
-    <div className="SiteBar__Main">
-    <SiteBar />
-    <Main />
-     </div>
-    <Footer />
-   
-   </div>
+    <div className="wrapp">
+      <Header />
+      <div className="wrapp__content">
+        <div className="content__siteBar">
+          <SiteBar />
+        </div>
+        <div className="content__main">
+          <Dialogs />
+          {/* <Main /> */}
+
+        </div>
+      </div>
+      <Footer />
+
+    </div>
   );
 }
 
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./Dialogs.module.css";
+
+
+const Dialogs=(props)=>{
+    return(
+<main className={s.dialogs}>
+<h2>Бесіди</h2>
+</main>
+    )
+}
+export default Dialogs;
