8e1c27dc8dd4b765e84bdebabc140f82fea8b369
Serey <zin4ik86@gmail.com>
2019-09-16 02:01:24 +0300

    Route +

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter,  Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
 import SiteBar from './components/SiteBar/SiteBar';
@@ -10,21 +11,26 @@ import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
-    <div className="wrapp">
-      <Header />
-      <div className="wrapp__content">
-        <div className="content__siteBar">
-          <SiteBar />
-        </div>
-        <div className="content__main">
-          <Dialogs />
-          {/* <Main /> */}
 
+    <BrowserRouter>
+      <div className="wrapp">
+        <Header />
+        <div className="wrapp__content">
+          <div className="content__siteBar">
+            <SiteBar />
+          </div>
+          <div className="content__main">
+            <Route path="/dialogs" component={Dialogs} />
+            {/* <Route path="/myProfil" components={Dialogs} /> */}
+            <Route path="/news" component={Main} />
+            {/* <Main /> */}
+
+          </div>
         </div>
+        <Footer />
+
       </div>
-      <Footer />
-
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/SiteBar/SiteBar.jsx
+++ src/components/SiteBar/SiteBar.jsx
@@ -7,10 +7,16 @@ const SiteBar = ()=>{
         <aside className="siteBar">
             <nav className="siteBar__nav">
                 <ul className="siteBar__list">
-                    <li className="siteBar__item"><a href="#" className="siteBar__link link">Мій профиль</a></li>
-                    <li className="siteBar__item"><a href="#" className="siteBar__link link">Повідомлення</a></li>
-                    <li className="siteBar__item"><a href="#" className="siteBar__link link">Моя діаспора</a></li>
-                    <li className="siteBar__item"><a href="#" className="siteBar__link link">АкунаМатата</a></li> 
+                    <li className="siteBar__item">
+                        <a href="/myProfil" className="siteBar__link link">Мій профиль</a></li>
+                    <li className="siteBar__item">
+                        <a href="/dialogs" className="siteBar__link link">Повідомлення</a></li>
+                    <li className="siteBar__item">
+                    <a href="/news" className="siteBar__link link">Новини</a></li>
+                    <li className="siteBar__item">
+                        <a href="#" className="siteBar__link link">Моя діаспора</a></li>
+                    <li className="siteBar__item">
+                        <a href="#" className="siteBar__link link">АкунаМатата</a></li> 
                 </ul>
             </nav>
             <div className="siteBar__none"></div>
